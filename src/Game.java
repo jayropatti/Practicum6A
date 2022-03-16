82efc4483b4136252d56d18f6ca31049f4953536
Thomharland <thomharland@gmail.com>
2022-03-16 14:02:38 +0100

    Test Thom Commit Versie 3

--- src/Game.java
+++ src/Game.java
@@ -24,12 +24,16 @@ public class Game {
     }
 
     public boolean equals(Object andereObject) {
-        if (andereObject.equals(toString())) {
-            return true;
+        boolean gelijkeObjecten = false;
 
-        } else {
-            return false;
+        if (andereObject instanceof Game) {
+            Game andereGame = (Game) andereObject;
+
+            if (this.naam.equals(andereGame.naam) && this.releaseJaar == andereGame.releaseJaar ) {
+                gelijkeObjecten = true;
+            }
         }
+        return gelijkeObjecten;
     }
     public String toString() {
         return "\n" + naam + ", uitgegeven in " + releaseJaar + "; nieuwprijs: €" + String.format("%.2f", nieuwprijs) + " nu voor: €" + String.format("%.2f", huidigeWaarde());
